c31389954afa9f4b520017d7a5a5938df312886f
Joey Atiba <joey.atiba@gmail.com>
2014-12-15 01:43:55 -0800

    alters the background style with the wallpaper, google font added, button backgrounds are now transparent as well

--- js/GamePlayController.js
+++ js/GamePlayController.js
@@ -13,9 +13,6 @@ function GamePlayController($firebase) {
 
 	self.sync = $firebase(ref).$asObject();
 
-
-
-
 	this.numSquares = [0,1,2,3,4,5,6,7,8];
 	
 	this.placeSquare = placeSquare;
@@ -28,7 +25,7 @@ function GamePlayController($firebase) {
 	
 	self.sync.xWins = 0;
 
-	self.sync.appleWins = 0;
+	self.sync.oWins = 0;
 
 	self.sync.catsGame = 0;
 
@@ -44,12 +41,12 @@ function GamePlayController($firebase) {
 
 	function placeSquare($index){
 		if(this.currentPlayer == 1 ){
-			this.numSquares[$index] = "x";
+			this.numSquares[$index] = "X";
 			this.currentPlayer = 2;
 
 		}
 		else {
-			this.numSquares[$index] = "apple";
+			this.numSquares[$index] = "O";
 			this.currentPlayer = 1;
 		}
 	}
@@ -61,96 +58,96 @@ function GamePlayController($firebase) {
 		if((this.numSquares[0] == this.numSquares[1]) && (this.numSquares[1] == this.numSquares[2])){
 			console.log(this.numSquares[0] + " wins!!");
 			this.declareWin = this.numSquares[0] + " wins!!";
-			if(this.numSquares[1] === "x"){
+			if(this.numSquares[1] === "X"){
 				self.sync.xWins = self.sync.xWins + 1;
 				self.sync.$save();
 			}
 			else {
-				self.sync.appleWins = self.sync.appleWins + 1;
+				self.sync.oWins = self.sync.oWins + 1;
 				self.sync.$save();
 			};
 		}
 		else if ((this.numSquares[3] == this.numSquares[4]) && (this.numSquares[4] == this.numSquares[5])){
 			console.log(this.numSquares[3] + " wins!!");
 			this.declareWin = this.numSquares[3] + " wins!!";
-			if(this.numSquares[4] === "x"){
+			if(this.numSquares[4] === "X"){
 				self.sync.xWins = self.sync.xWins + 1;
 				self.sync.$save();
 			}
 			else {
-				self.sync.appleWins = self.sync.appleWins + 1;
+				self.sync.oWins = self.sync.oWins + 1;
 				self.sync.$save();
 			};
 		}
 		else if((this.numSquares[6] == this.numSquares[7]) && (this.numSquares[7] == this.numSquares[8])){
 			console.log(this.numSquares[6] + " wins!!");
 			this.declareWin = this.numSquares[7] + " wins!!";
-			if(this.numSquares[7] === "x"){
+			if(this.numSquares[7] === "X"){
 				self.sync.xWins = self.sync.xWins + 1;
 				self.sync.$save();
 			}
 			else {
-				self.sync.appleWins = self.sync.appleWins + 1;
+				self.sync.oWins = self.sync.oWins + 1;
 				self.sync.$save();
 			};
 		}
 		else if ((this.numSquares[0] == this.numSquares[3]) && (this.numSquares[3] == this.numSquares[6])){
 			console.log(this.numSquares[0] + " wins!!");
 			this.declareWin = this.numSquares[0] + " wins!!";
-			if(this.numSquares[3] === "x"){
+			if(this.numSquares[3] === "X"){
 				self.sync.xWins = self.sync.xWins + 1;
 				self.sync.$save();
 			}
 			else {
-				self.sync.appleWins = self.sync.appleWins + 1;
+				self.sync.oWins = self.sync.oWins + 1;
 				self.sync.$save();
 			};
 		}
 		else if ((this.numSquares[1] == this.numSquares[4]) && (this.numSquares[4] == this.numSquares[7])){
 			console.log(this.numSquares[1] + " wins!!");
 			this.declareWin = this.numSquares[1] + " wins!!";
-			if(this.numSquares[4] === "x"){
+			if(this.numSquares[4] === "X"){
 				self.sync.xWins = self.sync.xWins + 1;
 				self.sync.$save();
 			}
 			else {
-				self.sync.appleWins = self.sync.appleWins + 1;
+				self.sync.oWins = self.sync.oWins + 1;
 				self.sync.$save();
 			};
 		}
 		else if ((this.numSquares[2] == this.numSquares[5]) && (this.numSquares[5] == this.numSquares[8])){
 			console.log(this.numSquares[2] + " wins!!");
 			this.declareWin = this.numSquares[2] + " wins!!";
-			if(this.numSquares[5] === "x"){
+			if(this.numSquares[5] === "X"){
 				self.sync.xWins = self.sync.xWins + 1;
 				self.sync.$save();
 			}
 			else {
-				self.sync.appleWins = self.sync.appleWins + 1;
+				self.sync.oWins = self.sync.oWins + 1;
 				self.sync.$save();
 			};
 		}
 		else if ((this.numSquares[0] == this.numSquares[4]) && (this.numSquares[4] == this.numSquares[8])){
 			console.log(this.numSquares[0] + " wins!!");
 			this.declareWin = this.numSquares[0] + " wins!!";
-			if(this.numSquares[4] === "x"){
+			if(this.numSquares[4] === "X"){
 				self.sync.xWins = self.sync.xWins + 1;
 				self.sync.$save();
 			}
 			else {
-				self.sync.appleWins = self.sync.appleWins + 1;
+				self.sync.oWins = self.sync.oWins + 1;
 				self.sync.$save();
 			};
 		}
 		else if ((this.numSquares[2] == this.numSquares[4]) && (this.numSquares[4] == this.numSquares[6])){
 			console.log(this.numSquares[2] + " wins!!");
 			this.declareWin = this.numSquares[2] + " wins!!";
-			if(this.numSquares[4] === "x"){
+			if(this.numSquares[4] === "X"){
 				self.sync.xWins = self.sync.xWins + 1;
 				self.sync.$save();
 			}
 			else {
-				self.sync.appleWins = self.sync.appleWins + 1;
+				self.sync.oWins = self.sync.oWins + 1;
 				self.sync.$save();
 			};
 
@@ -170,17 +167,6 @@ function GamePlayController($firebase) {
 		this.numSquares = [0,1,2,3,4,5,6,7,8];
 		this.counter = 0;
 		this.declareWin = null;
-		function erase($index){
-		if(this.currentPlayer == 1 ){
-			this.numSquares[$index] = "empty";
-			this.currentPlayer = 2;
-
-		}
-		else {
-			this.numSquares[$index] = "empty";
-			this.currentPlayer = 1;
-		}
-	}
 
 		}
 
