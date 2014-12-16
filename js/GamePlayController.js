69e212b3aeb3befd49d61ecb0b47a31a00e21c93
Joey Atiba <joey.atiba@gmail.com>
2014-12-15 18:28:35 -0800

    the firebase is connected completely and players can play each other in separate locations

--- js/GamePlayController.js
+++ js/GamePlayController.js
@@ -9,154 +9,159 @@ function GamePlayController($firebase) {
 	
 	var self = this;
 
-	var ref = new Firebase("https://tttizzo.firebaseio.com/scoreboard");
+	var ref = new Firebase("https://tttizzo.firebaseio.com");
 
 	self.sync = $firebase(ref).$asObject();
 
-	this.numSquares = [0,1,2,3,4,5,6,7,8];
+	self.sync.numSquares = [0,1,2,3,4,5,6,7,8];
 	
 	this.placeSquare = placeSquare;
 
 	this.checkWin = checkWin;
 
-	this.currentPlayer = 1;
+	self.sync.currentPlayer = 1;
 
 	this.declareWin = null;
 	
+	
+
+	this.counter = 0;
+
+	this.newGame = newGame;
+
+
+
 	self.sync.xWins = 0;
 
 	self.sync.oWins = 0;
 
 	self.sync.catsGame = 0;
 
-	this.counter = 0;
-
-	this.newGame = newGame;
-
 	self.sync.$save();
 
 
 	
-
-
+// set puts square onto the board
 	function placeSquare($index){
-		if(this.currentPlayer == 1 ){
-			this.numSquares[$index] = "X";
-			this.currentPlayer = 2;
-
+		// player 1
+		if(self.sync.currentPlayer == 1 ){
+			self.sync.numSquares[$index] = "X";
+			self.sync.currentPlayer = 2;
 		}
+		// player 2
 		else {
-			this.numSquares[$index] = "O";
-			this.currentPlayer = 1;
+			self.sync.numSquares[$index] = "O";
+			self.sync.currentPlayer = 1;
 		}
+		self.sync.$save(self.sync.numSquares);
 	}
 
 	
 
 	function checkWin(){
 		console.log("clicked");
-		if((this.numSquares[0] == this.numSquares[1]) && (this.numSquares[1] == this.numSquares[2])){
-			console.log(this.numSquares[0] + " wins!!");
-			this.declareWin = this.numSquares[0] + " wins!!";
-			if(this.numSquares[1] === "X"){
+		if((self.sync.numSquares[0] == self.sync.numSquares[1]) && (self.sync.numSquares[1] == self.sync.numSquares[2])){
+			console.log(self.sync.numSquares[0] + " wins!!");
+			this.declareWin = self.sync.numSquares[0] + " wins!!";
+			if(self.sync.numSquares[1] === "X"){
 				self.sync.xWins = self.sync.xWins + 1;
-				self.sync.$save();
+				self.sync.$save(self.sync.xWins);
 			}
 			else {
 				self.sync.oWins = self.sync.oWins + 1;
-				self.sync.$save();
+				self.sync.$save(self.sync.oWins);
 			};
 		}
-		else if ((this.numSquares[3] == this.numSquares[4]) && (this.numSquares[4] == this.numSquares[5])){
-			console.log(this.numSquares[3] + " wins!!");
-			this.declareWin = this.numSquares[3] + " wins!!";
-			if(this.numSquares[4] === "X"){
+		else if ((self.sync.numSquares[3] == self.sync.numSquares[4]) && (self.sync.numSquares[4] == self.sync.numSquares[5])){
+			console.log(self.sync.numSquares[3] + " wins!!");
+			this.declareWin = self.sync.numSquares[3] + " wins!!";
+			if(self.sync.numSquares[4] === "X"){
 				self.sync.xWins = self.sync.xWins + 1;
-				self.sync.$save();
+				self.sync.$save(self.sync.xWins);
 			}
 			else {
 				self.sync.oWins = self.sync.oWins + 1;
-				self.sync.$save();
+				self.sync.$save(self.sync.oWins);
 			};
 		}
-		else if((this.numSquares[6] == this.numSquares[7]) && (this.numSquares[7] == this.numSquares[8])){
-			console.log(this.numSquares[6] + " wins!!");
-			this.declareWin = this.numSquares[7] + " wins!!";
-			if(this.numSquares[7] === "X"){
+		else if((self.sync.numSquares[6] == self.sync.numSquares[7]) && (self.sync.numSquares[7] == self.sync.numSquares[8])){
+			console.log(self.sync.numSquares[6] + " wins!!");
+			this.declareWin = self.sync.numSquares[7] + " wins!!";
+			if(self.sync.numSquares[7] === "X"){
 				self.sync.xWins = self.sync.xWins + 1;
-				self.sync.$save();
+				self.sync.$save(self.sync.xWins);
 			}
 			else {
 				self.sync.oWins = self.sync.oWins + 1;
-				self.sync.$save();
+				self.sync.$save(self.sync.oWins);
 			};
 		}
-		else if ((this.numSquares[0] == this.numSquares[3]) && (this.numSquares[3] == this.numSquares[6])){
-			console.log(this.numSquares[0] + " wins!!");
-			this.declareWin = this.numSquares[0] + " wins!!";
-			if(this.numSquares[3] === "X"){
+		else if ((self.sync.numSquares[0] == self.sync.numSquares[3]) && (self.sync.numSquares[3] == self.sync.numSquares[6])){
+			console.log(self.sync.numSquares[0] + " wins!!");
+			this.declareWin = self.sync.numSquares[0] + " wins!!";
+			if(self.sync.numSquares[3] === "X"){
 				self.sync.xWins = self.sync.xWins + 1;
-				self.sync.$save();
+				self.sync.$save(self.sync.xWins);
 			}
 			else {
 				self.sync.oWins = self.sync.oWins + 1;
-				self.sync.$save();
+				self.sync.$save(self.sync.oWins);
 			};
 		}
-		else if ((this.numSquares[1] == this.numSquares[4]) && (this.numSquares[4] == this.numSquares[7])){
-			console.log(this.numSquares[1] + " wins!!");
-			this.declareWin = this.numSquares[1] + " wins!!";
-			if(this.numSquares[4] === "X"){
+		else if ((self.sync.numSquares[1] == self.sync.numSquares[4]) && (self.sync.numSquares[4] == self.sync.numSquares[7])){
+			console.log(self.sync.numSquares[1] + " wins!!");
+			this.declareWin = self.sync.numSquares[1] + " wins!!";
+			if(self.sync.numSquares[4] === "X"){
 				self.sync.xWins = self.sync.xWins + 1;
-				self.sync.$save();
+				self.sync.$save(self.sync.xWins);
 			}
 			else {
 				self.sync.oWins = self.sync.oWins + 1;
-				self.sync.$save();
+				self.sync.$save(self.sync.oWins);
 			};
 		}
-		else if ((this.numSquares[2] == this.numSquares[5]) && (this.numSquares[5] == this.numSquares[8])){
-			console.log(this.numSquares[2] + " wins!!");
-			this.declareWin = this.numSquares[2] + " wins!!";
-			if(this.numSquares[5] === "X"){
+		else if ((self.sync.numSquares[2] == self.sync.numSquares[5]) && (self.sync.numSquares[5] == self.sync.numSquares[8])){
+			console.log(self.sync.numSquares[2] + " wins!!");
+			this.declareWin = self.sync.numSquares[2] + " wins!!";
+			if(self.sync.numSquares[5] === "X"){
 				self.sync.xWins = self.sync.xWins + 1;
-				self.sync.$save();
+				self.sync.$save(self.sync.xWins);
 			}
 			else {
 				self.sync.oWins = self.sync.oWins + 1;
-				self.sync.$save();
+				self.sync.$save(self.sync.oWins);
 			};
 		}
-		else if ((this.numSquares[0] == this.numSquares[4]) && (this.numSquares[4] == this.numSquares[8])){
-			console.log(this.numSquares[0] + " wins!!");
-			this.declareWin = this.numSquares[0] + " wins!!";
-			if(this.numSquares[4] === "X"){
+		else if ((self.sync.numSquares[0] == self.sync.numSquares[4]) && (self.sync.numSquares[4] == self.sync.numSquares[8])){
+			console.log(self.sync.numSquares[0] + " wins!!");
+			this.declareWin = self.sync.numSquares[0] + " wins!!";
+			if(self.sync.numSquares[4] === "X"){
 				self.sync.xWins = self.sync.xWins + 1;
-				self.sync.$save();
+				self.sync.$save(self.sync.xWins);
 			}
 			else {
 				self.sync.oWins = self.sync.oWins + 1;
-				self.sync.$save();
+				self.sync.$save(self.sync.oWins);
 			};
 		}
-		else if ((this.numSquares[2] == this.numSquares[4]) && (this.numSquares[4] == this.numSquares[6])){
-			console.log(this.numSquares[2] + " wins!!");
-			this.declareWin = this.numSquares[2] + " wins!!";
-			if(this.numSquares[4] === "X"){
+		else if ((self.sync.numSquares[2] == self.sync.numSquares[4]) && (self.sync.numSquares[4] == self.sync.numSquares[6])){
+			console.log(self.sync.numSquares[2] + " wins!!");
+			this.declareWin = self.sync.numSquares[2] + " wins!!";
+			if(self.sync.numSquares[4] === "X"){
 				self.sync.xWins = self.sync.xWins + 1;
-				self.sync.$save();
+				self.sync.$save(self.sync.xWins);
 			}
 			else {
 				self.sync.oWins = self.sync.oWins + 1;
-				self.sync.$save();
+				self.sync.$save(self.sync.oWins);
 			};
 
 		}
 		this.counter = this.counter + 1;
-		if(this.counter >= (this.numSquares.length) && this.declareWin == null) {
+		if(this.counter >= (self.sync.numSquares.length) && this.declareWin == null) {
 			this.declareWin = "Cat's Game!!!";
 			self.sync.catsGame = self.sync.catsGame + 1;
-			self.sync.$save();
+			self.sync.$save(self.sync.catsGame);
 		}
 	}
 
@@ -164,9 +169,10 @@ function GamePlayController($firebase) {
 
 	function newGame(){
 		
-		this.numSquares = [0,1,2,3,4,5,6,7,8];
+		self.sync.numSquares = [0,1,2,3,4,5,6,7,8];
 		this.counter = 0;
 		this.declareWin = null;
+		self.sync.$save();
 
 		}
 
